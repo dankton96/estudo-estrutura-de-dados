0648a93b238d7e601f1a17a124b0f1575bad1ba9
dankton96 <dankton96@hotmail.com>
2015-11-26 18:50:20 -0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pilha.h"
+
+void main()
+{
+    printf("Numero de elementos a incluir na pilha:");
+    int op;
+    scanf("%d",&op);
+    TipoPilha p1,*p1aux;
+    p1aux=&p1;
+    for(int i=1;i<=op;i++)
+    {
+        int num;
+        system("cls");
+        printf("Numero: ");
+        scanf("%d",&num);
+        empilha(p1aux,num);
+    }
+    mostrapilha(p1aux);
+}
